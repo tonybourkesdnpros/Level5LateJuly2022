f2ad5c75721e4afc8c067d07446e321012492e2c
arista <atd-help@arista.com>
2022-07-27 11:39:08 +0000

    Commiting without staging

--- Python/demo.py
+++ Python/demo.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import yaml 
+
 # variables (simple, complex), for loops
 
 # Simple variables (integers, strings, Booleans)
@@ -17,7 +19,7 @@ greeting_2 = "world!"
 # print(greeting_1, greeting_2)
 
 # Complex variables (lists, dictionaries)
-
+## Lists
 planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
 
 # for planet in planets:
@@ -28,3 +30,17 @@ spines = ['192.168.101.101', '192.168.101.102', '192.168.101.103']
 # for neighbor in spines:
 #     print("neighbor", neighbor, "peer group EVPN")
 
+## Dictionaries
+
+file = open('underlay.yml', 'r')
+
+underlay = yaml.safe_load(file)
+
+for switch in underlay['devices']:
+    print('Syntax for:', switch)
+    for iface in underlay['devices'][switch]['interfaces']:
+        ip = underlay['devices'][switch]['interfaces'][iface]
+        print('interface', iface)
+        print('  ip address', ip)
+        if 'Ethernet' in iface:
+            print('  no switchport')
