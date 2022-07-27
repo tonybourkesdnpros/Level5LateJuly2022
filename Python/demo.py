90990de480ee4c7088cb0589d17ffb1f035380ea
arista <atd-help@arista.com>
2022-07-27 08:09:20 +0000

    Python demo

---
+++ Python/demo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# variables (simple, complex), for loops
+
+# Simple variables (integers, strings, Booleans)
+
+## Integers
+i = 1
+p = 3
+
+e = p + i
+
+## Strings
+
+greeting_1 = "Hello, "
+greeting_2 = "world!"
+# print(greeting_1, greeting_2)
+
+# Complex variables (lists, dictionaries)
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+# for planet in planets:
+#    print(planet, "is a planet")
+
+spines = ['192.168.101.101', '192.168.101.102', '192.168.101.103']
+
+# for neighbor in spines:
+#     print("neighbor", neighbor, "peer group EVPN")
+
